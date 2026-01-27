88aeb4f17750eb878632ef1ad8a3a6d7e352996d
samedit66 <samedit66@yandex.ru>
2026-01-28 01:54:38 +0300

    [refactor]: CLI

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -20,18 +20,20 @@ defmodule Issues.CLI do
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
   def parse_args(argv) do
-    {parsed, args, _unknown} =
+    {_parsed, args, _unknown} =
       OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
 
-    case {parsed, args} do
-      {[help: true], _} ->
-        :help
-
-      {_, [user, project, count]} ->
-        {user, project, String.to_integer(count)}
-
-      {_, [user, project]} ->
-        {user, project, @default_count}
-    end
+    do_parse_args(args)
   end
+
+  defp do_parse_args([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  defp do_parse_args([user, project]) do
+    {user, project, @default_count}
+  end
+
+  # bad arg or --help
+  defp do_parse_args(_other), do: :help
 end
