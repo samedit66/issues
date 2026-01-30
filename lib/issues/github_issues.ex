70c387641874e51605680903c300fdb9f95c2720
samedit66 <samedit66@yandex.ru>
2026-01-30 16:50:40 +0300

    [feat]: logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,9 +1,13 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
   @github_url Application.compile_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -14,6 +18,9 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code = #{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       check_for_error(status_code),
       Poison.Parser.parse!(body)
